4b80a336faccd48052740ef32418556da17ce104
Deepa KR <deepasujatha19@gmail.com>
2026-09-15 11:01:37 +0530

    python String methods

---
+++ string_methods.py
@@ -0,0 +1,42 @@
+st =" Deepa e "
+str1 ="Deepa"
+
+l = "nature world"
+M = ['python','program','language']
+str2 = "Kr"
+a = "123425"
+b = "f"
+#strip
+print(f"1.{st.strip()}")
+#lower
+print(f"2.{str1.lower()}")
+# upper
+print(f"3.{str1.upper()}")
+#Capatalize
+print(f"4.{str1.capitalize()}")
+# concatination
+print(f"5.{str1 + str2}")
+#repetation
+print(f"6.{str1 * 5}")
+#startswith
+print(f"7.{str1.startswith('de')}")
+#endswith
+print(f"8.{str1.endswith('pa')}")
+#title
+print(f"9.{l.title()}")
+#find
+print(f"10.{l.find('r')}")
+#replace
+print(f"11.{l.replace('world','earth')}")
+#split
+print(f"12.{l.split()}")
+#isdigit
+print(f"13.{a.isdigit()}")
+#isalpha
+print(f"14.{b.isalpha()}")
+#isalnum
+print(f"15.{a.isalnum()}")
+#join
+print(f"16s.{"_".join(M)}")
+
+
